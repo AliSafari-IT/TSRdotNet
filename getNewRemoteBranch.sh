e4024968e13b1574661e8ebda5b1a739ae0d410f
Ali SAFARI <58768873+AliSafari-IT@users.noreply.github.com>
2020-07-17 08:42:52 +0200

    Branch CSVLoader (07/17/20 08:42:52): Energy Time Series Modeling in C# using rDotNet

--- getNewRemoteBranch.sh
+++ getNewRemoteBranch.sh
@@ -36,6 +36,7 @@ git branch -a
 echo "\n\n Which branch would you like to pull? "
 read thisBranch
 
+git branch $thisBranch
 git branch --set-upstream-to=origin/$thisBranch $thisBranch
 git pull
 printf "\n \n"
